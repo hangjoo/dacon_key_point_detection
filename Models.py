54c8285aad418bc48daed0e418e7f8314b7d8733
JangHyungJoo <hyungjoo1470@gmail.com>
2021-03-15 02:24:31 +0900

    add create_model function.

--- Models.py
+++ Models.py
@@ -1,2 +1,12 @@
 import torch.nn as nn
 from torchvision import models
+
+
+def create_model(class_num: int, model_name: str):
+    if model_name == "resnet":
+        model_ft = models.resnet34(pretrained=True)
+
+        model_in_features = model_ft.fc.in_features
+        model_ft.fc = nn.linear(model_in_features, class_num)
+
+        return model_ft
